dd76ad40a23d1cd0ee81b26ba25397b5b4c61256
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2021-06-10 10:42:27 +0300

    refactoring

--- src/test/java/ru/schernolyas/testtask/service/StatementParserTest.java
+++ src/test/java/ru/schernolyas/testtask/service/StatementParserTest.java
@@ -8,13 +8,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import ru.schernolyas.testtask.TestConfig;
 import ru.schernolyas.testtask.dto.StatementInfo;
+import ru.schernolyas.testtask.util.BigDecimalUtil;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -38,14 +37,11 @@ public class StatementParserTest {
         StatementInfo statementInfo = statementParser.parse(new BufferedReader(new InputStreamReader(io, "UTF-8")));
         assertNotNull(statementInfo);
         assertNotNull(statementInfo.getInitialBalance());
-        assertEquals(new BigDecimal(12583.78).setScale(2, RoundingMode.HALF_UP), statementInfo.getInitialBalance());
+        assertEquals(BigDecimalUtil.build(12583.78), statementInfo.getInitialBalance());
         assertNotNull(statementInfo.getFinalBalance());
-        assertEquals(new BigDecimal(13731.78).setScale(2, RoundingMode.HALF_UP), statementInfo.getFinalBalance());
+        assertEquals(BigDecimalUtil.build(13731.78), statementInfo.getFinalBalance());
         assertNotNull(statementInfo.getStatementSummary());
-        assertEquals(new BigDecimal(1250.00).setScale(2, RoundingMode.HALF_UP), statementInfo.getStatementSummary().getCredit());
-        assertEquals(new BigDecimal(102.00).setScale(2, RoundingMode.HALF_UP), statementInfo.getStatementSummary().getDebit());
-
+        assertEquals(BigDecimalUtil.build(1250.00), statementInfo.getStatementSummary().getCredit());
+        assertEquals(BigDecimalUtil.build(102.00), statementInfo.getStatementSummary().getDebit());
     }
-
-
 }
